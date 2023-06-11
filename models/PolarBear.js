027a2678d77b628dc3fcd6cf75c776acd71a0ad3
John Henry Hopkins <jhhopkins13@gmail.com>
2023-06-11 08:14:00 -0500

    models

---
+++ models/PolarBear.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+
+PolarBear.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    habitat: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    modelName: 'PolarBear',
+    timestamps: true,
+    underscored: true
+  }
+);
+
+module.exports = PolarBear;
