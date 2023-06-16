5ab2906835a858272ca28de76505e0f0291b9ca4
Tranel <tranel@Tranels-MacBook-Air.local>
2023-06-15 19:47:41 -0500

    Added login function

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  'ecopaw_db',
+  'root',
+  'Peyton3232!',
   {
     host: 'localhost',
     dialect: 'mysql',
